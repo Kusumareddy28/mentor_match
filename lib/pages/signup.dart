15aeca92b4ae6d6e3930d86965b32b0fa65216b1
Kusumareddy28 <kusumareddy28@gmail.com>
2024-05-04 18:44:45 -0700

    Adding If already registered, login functionality

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,6 +1,244 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+
+// class SignUpPage extends StatefulWidget {
+//   @override
+//   _SignUpPageState createState() => _SignUpPageState();
+// }
+
+// class _SignUpPageState extends State<SignUpPage> {
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+//   final TextEditingController _fullNameController = TextEditingController();
+//   final TextEditingController _profilePicUrlController =
+//       TextEditingController();
+//   // Add the new controllers and variables
+//   final TextEditingController _dobController = TextEditingController();
+//   final TextEditingController _professionController = TextEditingController();
+//   String _gender = 'Female'; // Default gender value
+//   String _role = 'mentee'; // Default role value
+
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sign Up'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16),
+//         child: ListView(
+//           children: [
+//             TextField(
+//               controller: _fullNameController,
+//               decoration: InputDecoration(labelText: 'Full Name'),
+//             ),
+//             TextField(
+//               controller: _emailController,
+//               decoration: InputDecoration(labelText: 'Email'),
+//             ),
+//             TextField(
+//               controller: _passwordController,
+//               decoration: InputDecoration(labelText: 'Password'),
+//               obscureText: true,
+//             ),
+
+
+//             // Add new TextField for Profession
+
+//             GestureDetector(
+//               onTap: () => _selectDate(context), // Open date picker on tap
+//               child: AbsorbPointer(
+//                 // to prevent manual editing
+//                 child: TextField(
+//                   controller: _dobController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Date of Birth',
+//                     hintText: 'Select your date of birth',
+//                     suffixIcon: Icon(Icons.calendar_today),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             TextField(
+//               controller: _professionController,
+//               decoration: InputDecoration(labelText: 'Profession'),
+//             ),
+//             ListTile(
+//               title: Text('Mentor'),
+//               leading: Radio(
+//                 value: 'mentor',
+//                 groupValue: _role,
+//                 onChanged: (value) {
+//                   setState(() {
+//                     _role = value.toString();
+//                   });
+//                 },
+//               ),
+//             ),
+//             ListTile(
+//               title: Text('Mentee'),
+//               leading: Radio(
+//                 value: 'mentee',
+//                 groupValue: _role,
+//                 onChanged: (value) {
+//                   setState(() {
+//                     _role = value.toString();
+//                   });
+//                 },
+//               ),
+//             ),
+//           Padding(
+//             padding: EdgeInsets.symmetric(vertical: 8.0),
+//             child: Text('Gender', style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
+//           ),
+//             ListTile(
+//               title: Text('Female'),
+//               leading: Radio(
+//                 value: 'Female',
+//                 groupValue: _gender,
+//                 onChanged: (value) {
+//                   setState(() {
+//                     _gender = value.toString();
+//                   });
+//                 },
+//               ),
+//             ),
+//             ListTile(
+//               title: Text('Male'),
+//               leading: Radio(
+//                 value: 'Male',
+//                 groupValue: _gender,
+//                 onChanged: (value) {
+//                   setState(() {
+//                     _gender = value.toString();
+//                   });
+//                 },
+//               ),
+//             ),
+//             TextField(
+//               controller: _profilePicUrlController,
+//               decoration:
+//                   InputDecoration(labelText: 'Profile Picture URL (optional)'),
+//             ),
+//             SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () => _signUpWithEmailAndPassword(context),
+//               child: Text('Sign Up'),
+//             ),
+//             SizedBox(height: 20),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Future<void> _selectDate(BuildContext context) async {
+//     final DateTime? picked = await showDatePicker(
+//       context: context,
+//       initialDate: DateTime.now(),
+//       firstDate: DateTime(1900),
+//       lastDate: DateTime.now(),
+//     );
+//     if (picked != null) {
+//       setState(() {
+//         // Format the date and show it in the text field
+//         _dobController.text = "${picked.month}/${picked.day}/${picked.year}";
+//       });
+//     }
+//   }
+
+//   Future<void> _signUpWithEmailAndPassword(BuildContext context) async {
+//     try {
+//       // Basic email validation
+//       if (!_emailController.text.contains('@') ||
+//           !_emailController.text.contains('.')) {
+//         showDialog(
+//           context: context,
+//           builder: (context) => AlertDialog(
+//             title: Text('Error'),
+//             content: Text('Please enter a valid email address.'),
+//             actions: [
+//               TextButton(
+//                 onPressed: () => Navigator.pop(context),
+//                 child: Text('OK'),
+//               ),
+//             ],
+//           ),
+//         );
+//         return;
+//       }
+
+//       UserCredential userCredential =
+//           await FirebaseAuth.instance.createUserWithEmailAndPassword(
+//         email: _emailController.text,
+//         password: _passwordController.text,
+//       );
+
+//       if (userCredential.user != null) {
+//         await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
+//         'fullName': _fullNameController.text.trim(),
+//         'email': _emailController.text.trim(),
+//         'profilePicUrl': _profilePicUrlController.text.isEmpty ? null : _profilePicUrlController.text.trim(),
+//         'dob': _dobController.text,  // Ensure the date format is correct
+//         'profession': _professionController.text.trim(),
+//         'gender': _gender,
+//         'role': _role,
+//       });
+
+//         // Navigate to login page upon successful sign-up
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(builder: (context) => LoginPage()),
+//         );
+//       }
+//     } on FirebaseAuthException catch (e) {
+//       // Handle Firebase authentication errors
+//       String errorMessage = 'An unexpected error occurred. Please try again.';
+//       if (e.code == 'weak-password') {
+//         errorMessage = 'The password provided is too weak.';
+//       } else if (e.code == 'email-already-in-use') {
+//         errorMessage = 'An account already exists for that email.';
+//       }
+//       showDialog(
+//         context: context,
+//         builder: (context) => AlertDialog(
+//           title: Text('Error'),
+//           content: Text(errorMessage),
+//           actions: [
+//             TextButton(
+//               onPressed: () => Navigator.pop(context),
+//               child: Text('OK'),
+//             ),
+//           ],
+//         ),
+//       );
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+// }
+
+// class LoginPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Login'),
+//       ),
+//       body: Center(
+//         child: Text('Login Page Placeholder'),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import './login.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -11,7 +249,9 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _fullNameController = TextEditingController();
-  final TextEditingController _profilePicUrlController = TextEditingController();
+  final TextEditingController _profilePicUrlController =
+      TextEditingController();
+  // Add the new controllers and variables
   final TextEditingController _dobController = TextEditingController();
   final TextEditingController _professionController = TextEditingController();
   String _gender = 'Female'; // Default gender value
@@ -19,6 +259,18 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Check if a user is already signed in
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      // If user is already signed in, navigate to login page
+      WidgetsBinding.instance?.addPostFrameCallback((_) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        );
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign Up'),
@@ -40,10 +292,7 @@ class _SignUpPageState extends State<SignUpPage> {
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
-            TextField(
-              controller: _profilePicUrlController,
-              decoration: InputDecoration(labelText: 'Profile Picture URL (optional)'),
-            ),
+            // Add new TextField for Profession
             GestureDetector(
               onTap: () => _selectDate(context), // Open date picker on tap
               child: AbsorbPointer(
@@ -88,7 +337,8 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             Padding(
               padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text('Gender', style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
+              child: Text('Gender',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
             ),
             ListTile(
               title: Text('Female'),
@@ -114,12 +364,33 @@ class _SignUpPageState extends State<SignUpPage> {
                 },
               ),
             ),
+            TextField(
+              controller: _profilePicUrlController,
+              decoration: InputDecoration(
+                  labelText: 'Profile Picture URL (optional)'),
+            ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => _signUpWithEmailAndPassword(context),
               child: Text('Sign Up'),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text(
+                'Already Registered? Login',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 40, 202, 214),
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -168,17 +439,16 @@ class _SignUpPageState extends State<SignUpPage> {
         password: _passwordController.text,
       );
 
-      // Add new user's additional information to Firestore
       if (userCredential.user != null) {
         await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
-          'fullName': _fullNameController.text.trim(),
-          'email': _emailController.text.trim(),
-          'profilePicUrl': _profilePicUrlController.text.isEmpty ? null : _profilePicUrlController.text.trim(),
-          'dob': _dobController.text,  // Ensure the date format is correct
-          'profession': _professionController.text.trim(),
-          'gender': _gender,
-          'role': _role,
-        });
+        'fullName': _fullNameController.text.trim(),
+        'email': _emailController.text.trim(),
+        'profilePicUrl': _profilePicUrlController.text.isEmpty ? null : _profilePicUrlController.text.trim(),
+        'dob': _dobController.text,  // Ensure the date format is correct
+        'profession': _professionController.text.trim(),
+        'gender': _gender,
+        'role': _role,
+      });
 
         // Navigate to login page upon successful sign-up
         Navigator.pushReplacement(
@@ -210,19 +480,5 @@ class _SignUpPageState extends State<SignUpPage> {
     } catch (e) {
       print(e);
     }
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: Text('Login Page Placeholder'),
-      ),
-    );
   }
 }
